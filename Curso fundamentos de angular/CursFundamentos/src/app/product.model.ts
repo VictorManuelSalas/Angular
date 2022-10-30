3218332c11fa7e8025a70b7905df15f44f2d9380
VictorManuelSalas <victorsalasnz@gmail.com>
2022-10-30 16:36:30 -0600

    ng for final

--- Curso fundamentos de angular/CursFundamentos/src/app/product.model.ts
+++ Curso fundamentos de angular/CursFundamentos/src/app/product.model.ts
@@ -1,1 +1,10 @@
-interface Product {}
+//Esto sirve pára determinar que componentes o elementos con sus tipos de datos
+//debe de tener el array al cual se le agregara esta interface
+
+
+//Si no tiene el export no se podra exportar
+export interface Product {
+    name: string;
+    price: number;
+    image: string;
+}
